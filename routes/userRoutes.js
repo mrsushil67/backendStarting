1fd269a0d461d9c900062e1a9cb68d8e92e4cd14
Sushil Kumar <142862724+mrsushil67@users.noreply.github.com>
2025-02-04 16:58:10 +0530

    add about route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -20,4 +20,18 @@ router.get('/profile',authenticatedUser,userController.userProfile);
 
 router.get('/logout',authenticatedUser,userController.userLogout);
 
+router.get('/about',(req, res)=>{
+    res.status(200).json({
+        message: "My name is Sushil, and I created this backend server using Node.js and Express.js to learn how to deploy applications. After building the server, I deployed it on Vercel to understand the deployment process and how to manage server-side applications in a cloud environment.",
+        steps: [
+            "npm install -g vercel",
+            "vercel login",
+            "cd path/to/your/project",
+            "create vercel.json in root and write code from google",
+            "vercel --prod"
+        ]
+    });
+   
+})
+
 module.exports = router;
